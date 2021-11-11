f8df7b4ac22d8267ba4fd3fcee1488750bd121ac
Mubarak Cc7 <iz-syed@users.noreply.github.com>
2021-11-11 21:32:55 +0530

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlankComponent } from './layouts/blank/blank.component';
+import { FullComponent } from './layouts/full/full.component';
+import { IndexComponent } from './pages/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path:'',
+  //   component:BlankComponent
+  // },
+  {
+    path:'',
+    component:FullComponent,
+    children: [
+      {
+        path:'',
+        component:IndexComponent
+      },
+      {
+        path:'index',
+        component:IndexComponent
+      } 
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BlankComponent } from './layouts/blank/blank.component';
+import { FullComponent } from './layouts/full/full.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlankComponent,
+    FullComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
